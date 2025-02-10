c61abfd3bc7f0245b45f716e4a75e22c031ad681
renzhongwei <renzhongwei@bytedance.com>
2025-02-10 22:48:26 +0800

    Initial commit

--- VideoWorld/tools/calvin_train.sh
+++ VideoWorld/tools/calvin_train.sh
@@ -25,5 +25,5 @@ python3 -m torch.distributed.launch \
     --master_addr=$MASTER_ADDR \
     --nproc_per_node=$GPUS \
     --master_port=$PORT \
-    $(dirname "$0")/test.py $CONFIG $CHECKPOINT --launcher pytorch ${@:3}
+    $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
 
